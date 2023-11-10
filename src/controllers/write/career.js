5e7a2e59a4a1b4f57e45ec9dbc4e142ae17d96c1
Mohammad Anann <mohammadannan@Mohammads-MacBook-Air.local>
2023-11-10 21:54:41 +0300

    Edited the controller write

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -6,9 +6,30 @@ const db = require('../../database');
 
 const Career = module.exports;
 
-Career.register = async (req, res) => {
-    const userData = req.body;
+Career.register = async function (req, res) {
+
+    const userData = req.query;
     try {
+        // Forward the request to the Flask application
+        // console.log(userData.student_id);
+        const flaskResponse = await fetch('http://127.0.0.1:5000/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Include any other necessary headers
+            },
+            body: JSON.stringify({
+            student_id: userData.student_id,
+            major: userData.major,
+            age: userData.age,
+            gender: userData.gender,
+            gpa: userData.gpa,
+            extra_curricular: userData.extra_curricular,
+            num_programming_languages: userData.num_programming_languages,
+            num_past_internships: userData.num_past_internships,
+            })
+        });
+
         const userCareerData = {
             student_id: userData.student_id,
             major: userData.major,
@@ -19,9 +40,14 @@ Career.register = async (req, res) => {
             num_programming_languages: userData.num_programming_languages,
             num_past_internships: userData.num_past_internships,
         };
-
-        userCareerData.prediction = Math.round(Math.random()); // TODO: Change this line to do call and retrieve actual candidate success prediction from the model instead of using a random number
-
+        
+        if (!flaskResponse.ok) {
+            throw new Error('Failed to get prediction from Flask app.');
+        }
+        
+        const predictionResult = await flaskResponse.json();
+        console.log(predictionResult);
+        userCareerData.prediction = predictionResult['good_employee'];
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
         res.json({});
